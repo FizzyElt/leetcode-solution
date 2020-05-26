1c869b3eb8ecb62f17c6eda260debe22923fd510
FizzyElt <a0979150560@gmail.com>
2020-05-26 22:42:52 +0800

    701 go js solution

---
+++ src/#701/solution.js
@@ -0,0 +1,18 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
+var insertIntoBST = function (root, val) {
+  if (!root) {
+    return new TreeNode(val, null, null)
+  }
+
+  if (root.val > val) {
+    root.left = insertIntoBST(root.left, val)
+  } else {
+    root.right = insertIntoBST(root.right, val)
+  }
+  return root
+}
