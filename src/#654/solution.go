b36d5bd42567b23db0a96dfa5bc4bfa377b683bf
FizzyElt <a0979150560@gmail.com>
2020-05-26 22:02:21 +0800

    init commit

---
+++ src/#654/solution.go
@@ -0,0 +1,14 @@
+package Solution
+
+//link : https://leetcode.com/problems/maximum-binary-tree/
+
+
+type TreeNode struct{
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+    
+}
