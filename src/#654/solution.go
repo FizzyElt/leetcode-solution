a3f9d520cdf983c87195ba78871ea5da05f96900
FizzyElt <a0979150560@gmail.com>
2020-05-26 22:13:18 +0800

    654 solution

--- src/#654/solution.go
+++ src/#654/solution.go
@@ -2,13 +2,32 @@ package Solution
 
 //link : https://leetcode.com/problems/maximum-binary-tree/
 
-
-type TreeNode struct{
-	Val int
-	Left *TreeNode
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
 func constructMaximumBinaryTree(nums []int) *TreeNode {
-    
+
+	return buildTree(nums, 0, len(nums)-1)
+}
+
+func buildTree(nums []int, start, end int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	maxIndex := 0
+	for i, v := range nums {
+		if v > nums[maxIndex] {
+			maxIndex = i
+		}
+	}
+
+	root := &TreeNode{Val: nums[maxIndex]}
+	root.Left = buildTree(nums[start:maxIndex], start, maxIndex-1)
+	root.Right = buildTree(nums[maxIndex+1:], maxIndex+1, end)
+	
+	return root
 }
